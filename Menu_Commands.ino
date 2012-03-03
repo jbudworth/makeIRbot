3e5084275e4be3f08b1a78fcba1e1bc8054329fd
James Budworth <j.budworth@gmail.com>
2012-03-03 19:38:17 +0800

    Bug fix for jog clashing with extruder.
    Signed-off-by: James Budworth <j.budworth@gmail.com>

---
+++ Menu_Commands.ino
@@ -0,0 +1,325 @@
+//Menu Structure
+void menuSetup() {
+
+  menu.getRoot().add(m_connect);      // <-- Connect
+  m_connect.addRight(m_flags);
+  m_flags.addRight(m_debug);
+  m_debug.addRight(m_debugtog);
+  m_connect.addAfter(m_temps);        // <-- Temperature
+  m_temps.addRight(m_heaters);
+  m_heaters.addRight(m_extruder_temp);
+  m_extruder_temp.addRight(m_hbp_temp);
+  m_temps.addAfter(m_file);           // <-- File
+  m_file.addRight(m_build);
+  m_file.addAfter(m_position);        // <-- Position
+  m_position.addRight(m_zero);
+  m_zero.addRight(m_jogx);
+  m_jogx.addRight(m_jogy);
+  m_jogy.addRight(m_jogz);
+  m_position.addAfter(m_extruder);    // <-- Extruder
+  m_extruder.addRight(m_extruder_dir);     
+  m_extruder_dir.addRight(m_extruder_rpm);     
+  m_extruder.addAfter(m_fan);		  // <-- Fan
+  m_fan.addAfter(m_reset);			  // <-- Reset
+
+  m_extruder_temp.addBefore(m_extruder_temp_plus);
+  m_extruder_temp.addAfter(m_extruder_temp_minus);
+
+  m_hbp_temp.addBefore(m_hbp_temp_plus);
+  m_hbp_temp.addAfter(m_hbp_temp_minus);
+
+  m_heaters.addBefore(m_enable_heaters);
+  m_heaters.addAfter(m_disable_heaters);
+
+  m_jogx.addBefore(m_jogx_plus);
+  m_jogx.addAfter(m_jogx_minus);
+
+  m_jogy.addBefore(m_jogy_plus);
+  m_jogy.addAfter(m_jogy_minus);
+
+  m_jogz.addBefore(m_jogz_plus);
+  m_jogz.addAfter(m_jogz_minus);
+
+  m_extruder_dir.addBefore(m_extruder_rev);
+  m_extruder_dir.addAfter(m_extruder_fwd);
+
+  m_extruder_rpm.addBefore(m_extruder_rpm_plus);
+  m_extruder_rpm.addAfter(m_extruder_rpm_minus);      
+
+  //  Loop between first and last menu items.
+  m_reset.addAfter(m_connect);
+  m_connect.addBefore(m_reset);
+
+}
+
+// =============== Connect Menu ===============
+void infoDisplay(){
+  lcd.setCursor(16,0); lcd.write((uint8_t) 4);
+  lcd.setCursor(19,0); lcd.write((uint8_t) 3);
+  lcd.setCursor(0,0);
+  if (bitRead(flags, 0)) {
+    for (int i = 0; i < 16; i++) {
+      if (machineName[i] != 0x00) {
+        lcd.write(machineName[i]);
+      }
+      else {
+        lcd.print(" ");
+      }
+    }
+  }
+  else {
+    lcd.print("<NO CONNECTION>");
+  }
+  clearLCD(1);
+  lcd.setCursor(0,1); lcd.print("MB Firmware v");lcd.print(MotherboardVersion,1);
+  clearLCD(2);
+  lcd.setCursor(0,2); lcd.print("EC Firmware v");lcd.print(ExtruderControllerVersion,1);
+
+}
+void flagDisplay(){
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(19,0); lcd.write(3);
+  lcd.setCursor(0, 1);
+  lcd.print("c"); 
+  lcd.print(bitRead(flags, 0));
+  lcd.print("b"); 
+  lcd.print(bitRead(flags, 1));
+  lcd.print("v"); 
+  lcd.print(bitRead(flags, 2));
+  lcd.print("-"); 
+  lcd.print(bitRead(flags, 3));
+  lcd.print("-"); 
+  lcd.print(bitRead(flags, 4));
+  lcd.print("-"); 
+  lcd.print(bitRead(flags, 5));
+  lcd.print("-"); 
+  lcd.print(bitRead(flags, 6));
+  lcd.print("d"); 
+  lcd.print(bitRead(flags, 7));
+}
+void debugDisplay(){
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(19,0); lcd.write(3);
+  lcd.setCursor(0, 1);
+//  for (int i = 0; i < responseLength; i++) {
+    for (int i = 0; i < 32; i++) {
+
+    lcd.write(serialIn[i]);
+  }
+}
+void debugtogDisplay(){
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(19,0); lcd.write(5);
+
+}
+
+// ============= Temperature Menu =============
+void tempDisplay(){
+  lcd.setCursor(16,0); lcd.write((uint8_t) 4);
+  lcd.setCursor(19,0); lcd.write((uint8_t) 3);
+  lcd.setCursor(1,1); lcd.print("Extruder "); printTemp(extTemp, 11, 1, 47);
+  printTemp(extTarget, 15, 1, 223);
+  lcd.setCursor(1,2); lcd.print("Platform "); printTemp(hbpTemp, 11, 2, 47);
+  printTemp(hbpTarget, 15, 2, 223);
+  clearLCD(3);
+}
+void heatersDisplay(){
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(19,0); lcd.write(3);
+  lcd.setCursor(5,1); lcd.print("Enable "); lcd.write((uint8_t) 0);  
+  lcd.setCursor(5,2); lcd.print("Disable"); lcd.write((uint8_t) 1);
+}
+void extruder_temp(){
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(19,0); lcd.write(3);
+  printTemp(setextTemp, 8, 2, 223); 
+  lcd.write((uint8_t) 4);
+
+}
+void hbp_temp(){
+  lcd.setCursor(18,0); lcd.write(2);
+  printTemp(sethbpTemp, 8, 2, 223);     
+  lcd.write((uint8_t) 4);
+}
+
+// ================ File Menu ================
+void fileSelect(){
+  lcd.setCursor(16,0); lcd.write((uint8_t) 4);
+  lcd.setCursor(17,0); lcd.write((uint8_t) 5);
+  lcd.setCursor(19,0); lcd.write((uint8_t) 3);
+  printFilename(); // Make sure the filename display is always updated
+}
+void fileBuild(){
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(17,0); lcd.write(5);
+
+  if (!bitRead(flags, 2)) { // Valid filename?
+    menu.moveLeft();
+  }
+}
+
+// ============== Position Menu ==============
+void positionDisplay(){
+  lcd.setCursor(16,0); lcd.write((uint8_t) 4);
+  lcd.setCursor(19,0); lcd.write((uint8_t) 3);
+  clearLCD(1); lcd.setCursor(5,1); lcd.print("X:"); printPos(0, 1, 9); lcd.setCursor(15,1); lcd.print("  ");  
+  clearLCD(2); lcd.setCursor(5,2); lcd.print("Y:"); printPos(1, 2, 9); lcd.setCursor(15,2); lcd.print("  ");  
+  clearLCD(3); lcd.setCursor(5,3); lcd.print("Z:"); printPos(2, 3, 9); lcd.setCursor(15,3); lcd.print("  ");
+}
+void zeroposDisplay(){
+  lcd.setCursor(17,0);  lcd.write(5);
+  lcd.setCursor(18,0);  lcd.write(2);
+  lcd.setCursor(19,0);  lcd.write(3);
+}
+void jogxDisplay(){
+  lcd.setCursor(0,0); lcd.print("  ");
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(19,0); lcd.write(3);
+  lcd.setCursor(5,2); lcd.print("X: "); printPos(0, 2, 8); lcd.setCursor(13,2); lcd.print("       ");
+  clearLCD(1);
+  lcd.setCursor(9,1); lcd.write((uint8_t) 0);
+  lcd.setCursor(9,3); lcd.write((uint8_t) 1);
+}
+void jogyDisplay(){
+  lcd.setCursor(0,0); lcd.print("  ");
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(19,0); lcd.write(3);
+  lcd.setCursor(5,2); lcd.print("Y: "); printPos(1, 2, 8); lcd.setCursor(13,2); lcd.print("       ");
+  clearLCD(1);
+  lcd.setCursor(9,1); lcd.write((uint8_t) 0);
+  lcd.setCursor(9,3); lcd.write((uint8_t) 1);
+}
+void jogzDisplay(){
+  lcd.setCursor(0,0); lcd.print("  ");
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(5,2); lcd.print("Z: "); printPos(2, 2, 8); lcd.setCursor(13,2); lcd.print("       ");
+  clearLCD(1);
+  lcd.setCursor(9,1); lcd.write((uint8_t) 0);
+  lcd.setCursor(9,3); lcd.write((uint8_t) 1);
+}
+
+// ============== Extruder Menu =============
+void extruder(){
+  lcd.setCursor(16,0); lcd.write((uint8_t) 4);
+  lcd.setCursor(19,0); lcd.write((uint8_t) 3); 
+}
+void extruder_dir(){
+  lcd.setCursor(0,0);  lcd.print("  ");
+  lcd.setCursor(17,0); lcd.write(5);
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(19,0); lcd.write(3);
+  lcd.setCursor(5,1);  lcd.print("Reverse "); lcd.write((uint8_t) 0);
+  lcd.setCursor(5,2);  lcd.print("Forward "); lcd.write((uint8_t) 1);
+  lcd.setCursor(2,3);  lcd.print("Run Duration: "); lcd.print(extruder_duration[extruder_duration_Index]);lcd.print("s  ");
+}
+void extruder_dir_rev(){
+  lcd.setCursor(0,0);  lcd.print("  ");
+  lcd.setCursor(18,0);       lcd.write((uint8_t) 1);
+  lcd.setCursor(6,1);        lcd.print("Reverse");   //   lcd.write((uint8_t) 0);
+}
+void extruder_dir_fwd(){
+  lcd.setCursor(0,0); lcd.print("  ");
+  lcd.setCursor(18,0); lcd.write((uint8_t) 0);
+  lcd.setCursor(6,2); lcd.print("Forward");   //   lcd.write((uint8_t) 1);
+
+}
+void extruder_rpm() {
+  lcd.setCursor(0,0); lcd.print("  ");
+  lcd.setCursor(18,0); lcd.write(2);
+  lcd.setCursor(5,2); lcd.print("RPM: ");   
+  lcd.print(extruder_RPM); //lcd.setCursor(13,2); lcd.print("             ");
+  clearLCD(1);
+  lcd.setCursor(9,1); lcd.write((uint8_t) 0);
+  lcd.setCursor(9,3); lcd.write((uint8_t) 1);
+
+}
+
+// ================ Fan Menu ================
+void fanDisplay(){
+  lcd.setCursor(16,0); lcd.write((uint8_t) 4);
+  lcd.setCursor(17,0); lcd.write((uint8_t) 5);
+
+}
+
+// ================ Reset Menu ================
+void resetDisplay(){
+  lcd.setCursor(16,0); lcd.write((uint8_t) 4);
+  lcd.setCursor(17,0); lcd.write((uint8_t) 5);
+}
+
+// ==========================================
+void clearLCD(int row) {
+  lcd.setCursor(0, row);
+  for (int i = 0; i < numCols; i++) {
+    lcd.print(" ");
+  }
+  lcd.setCursor(0, row);
+}
+
+void printTemp(uint8_t temp, int col, int row, int symbol) {
+  lcd.setCursor(col, row);
+  if(temp < 10) { 
+    lcd.print("00");
+  }
+  else if(temp < 100) { 
+    lcd.print("0");
+  }
+  lcd.print(temp, DEC);
+  lcd.print((char) symbol);
+}
+
+void printPos(int axis, int row, int col) {
+  lcd.setCursor(col, row);
+  long steps = read32(axis);
+  float decPos = 0;
+  switch (axis) {
+  case 0: 
+    decPos = steps / xSteps; 
+    break;
+  case 1: 
+    decPos = steps / ySteps; 
+    break;
+  case 2: 
+    decPos = steps / zSteps; 
+    break;
+  }
+  // Handle formatting negative numbers
+  if (decPos < 0 && decPos > -10) {
+    lcd.print(" ");  
+  }
+  else if (decPos < 10) {
+    lcd.print("  ");
+  }
+  else if (decPos < 100) {
+    lcd.print(" ");
+  }
+  lcd.println(decPos, 2); // was DEC);
+  }
+
+float getPos(int axis){
+  long steps = read32(axis);
+  float decPos = 0;
+  switch (axis) {
+  case 0: 
+    decPos = steps / xSteps; 
+    break;
+  case 1: 
+    decPos = steps / ySteps; 
+    break;
+  case 2: 
+    decPos = steps / zSteps; 
+    break;
+  }
+  return decPos;
+}
+
+float calcVersion(uint16_t H, uint16_t L){
+  float version;
+  uint16_t ver = ((H << 8) + L);
+  uint16_t temp1 = ver / 100;
+  uint16_t temp2 = ver % 100;
+  float temp3 = (float)temp2/10;
+  version = temp1 + temp3;
+  return version;
+}
+
---
+++ Serial_Commands.ino
@@ -0,0 +1,284 @@
+// For all details on the Makerbot serial protocol see the
+// Reprap Generation 3 Protocol Specification v1 document
+// http://docs.google.com/View?docid=dgzjfrcc_2gm84ggdb
+/*  Not used
+void queryMakerbotInfo() { // Fetch build name of machine (Cupcake) firmware build name
+  uint8_t data[]= {0x14, 0x18, 0x00};
+  sendBytesWithCRC(data, sizeof(data));
+}
+*/
+void getVersion(){  //  Mimic version 29 of RepG while asking for Mobo firmware version
+  uint8_t data[] = {0x00, 0x1D, 0x00};
+  sendBytesWithCRC(data, sizeof(data));   
+}
+
+void initMakerbot(){
+  uint8_t data[] = {0x01};
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void getToolVersion(){
+  uint8_t data[] = {0x0A,0x00,0x00};
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void queryMachineName() { // Read 16 chars from EEPROM at Offset 32  
+  uint8_t data[] = {0x0C, 0x20, 0x00, 0x10};
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void fetchFirstFilename() {
+  uint8_t data[]= {0x12, 0x01};
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void fetchNextFilename() {
+  uint8_t data[]= {0x12, 0x00};
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void motor1DIR(bool dir){
+  uint8_t data[]= {0x0A, 0x00, 0x08, 0x00};
+  if (dir == true){
+    data[3] = 0x01;
+  }
+  else { data[3] = 0x00;}
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void motor1EN(bool en){
+  uint8_t data[]= {0x0A, 0x00, 0x0A, 0x00};
+  if (en == true){
+    data[3] = 0x01;
+  }
+  else { data[3] = 0x00;}
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void motor1RPM(float rpm){
+  uint8_t data[] = {0x0A,0x00,0x06,0x00,0x00,0x00,0x00};
+  uint32_t temp = (rpm * 60 * 1000000);
+  data[3] = (temp & 0x000000FF);
+  data[4] = (temp & 0x0000FF00)>>8;
+  data[5] = (temp & 0x00FF0000)>>16;
+  data[6] = (temp & 0xFF000000)>>24;
+  sendBytesWithCRC(data, 7);
+}
+
+void motor1PWM(uint8_t PWM){
+uint8_t data[] = {0x0A,0x00,0x04,0x00};
+data[3] = PWM;
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void motor1_DDA(float RPM, int runTime){
+  uint8_t data[] = {0x26, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  uint32_t temp = (1000000/(aSteps/60)) / RPM;  // uSecs between steps
+  data[1] = (temp & 0x000000FF);
+  data[2] = (temp & 0x0000FF00)>>8;
+  data[3] = (temp & 0x00FF0000)>>16;
+  data[4] = (temp & 0xFF000000)>>24;
+  data[5] = data[1]; 
+  data[6] = data[2];
+  data[7] = data[3];
+  data[8] = data[4];
+  uint32_t steps = (aSteps/60) * RPM * runTime; //Number of steps to take
+  data[9] =  (steps & 0x000000FF);
+  data[10] = (steps & 0x0000FF00)>>8;
+  data[11] = (steps & 0x00FF0000)>>16;
+  data[12] = (steps & 0xFF000000)>>24;
+  sendBytesWithCRC(data, 13);
+}
+
+void extDirEn(bool dir, bool enable){
+//  0 = false,false = CCW/Off
+//  1 = false,true = CCW/On
+//  2 = true,false = CW/Off
+//  3 = true,true = CW/On
+  uint8_t data[]= {0x0A, 0x00, 0x0A, 0x00};
+  if (dir == true){data[3] = 0b00000010;} 
+  if (enable == true){data[3] = data[3] | 0b00000001;}
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void setTemp(uint8_t toolIndex, uint8_t tooltemp) {
+  uint8_t data[]= {    0x0A, 0x00, toolIndex, tooltemp    };
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void getTemp(uint8_t toolIndex) {
+  uint8_t data[]= {    0x0A, 0x00, toolIndex    };
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void getPosition() {
+  uint8_t data[] = {0x04};
+    sendBytesWithCRC(data, 1);
+}
+
+void setPosition(float x, float y, float z) { // Hardcoded to X0 Y0 Z0        mm * axis steps
+  uint8_t data[] = {0x82, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  long temp = (x * xSteps);
+  data[4] = (temp & 0xFF000000)>>24;
+  data[3] = (temp & 0x00FF0000)>>16;
+  data[2] = (temp & 0x0000FF00)>>8;
+  data[1] = (temp & 0x000000FF);
+  temp = (y * ySteps);
+  data[8] = (temp & 0xFF000000)>>24;
+  data[7] = (temp & 0x00FF0000)>>16;
+  data[6] = (temp & 0x0000FF00)>>8;
+  data[5] = (temp & 0x000000FF);
+  temp = (z * zSteps);
+  data[12] = (temp & 0xFF000000)>>24;
+  data[11] = (temp & 0x00FF0000)>>16;
+  data[10] = (temp & 0x0000FF00)>>8;
+  data[9] = (temp & 0x000000FF);
+  sendBytesWithCRC(data, 13);
+}
+
+void queuePoint(float x, float y, float z) {
+  uint8_t data[] = {0x81, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+  long temp = (x * xSteps);
+  data[1] = (temp & 0x000000FF);
+  data[2] = (temp & 0x0000FF00)>>8;
+  data[3] = (temp & 0x00FF0000)>>16;
+  data[4] = (temp & 0xFF000000)>>24;
+  temp = (y * ySteps);
+  data[5] = (temp & 0x000000FF);
+  data[6] = (temp & 0x0000FF00)>>8;
+  data[7] = (temp & 0x00FF0000)>>16;
+  data[8] = (temp & 0xFF000000)>>24;
+  temp = (z * zSteps);
+  data[9] =  (temp & 0x000000FF);
+  data[10] = (temp & 0x0000FF00)>>8;
+  data[11] = (temp & 0x00FF0000)>>16;
+  data[12] = (temp & 0xFF000000)>>24;
+  // Delta = 250 uS per step adjust as needed
+  data[13] = 0xFA;
+  data[14] = 0x00;
+  data[15] = 0x00;
+  data[16] = 0x00;
+  sendBytesWithCRC(data, 17);
+  delay(50);
+  disableSteppers();
+}
+
+void getExtendedPosition(){
+  uint8_t data[] = {0x15};
+    sendBytesWithCRC(data, 1);
+}
+
+void queueXPointNS(float rpm, uint32_t duration, bool dir){
+  uint8_t data0[] = {0x88, 0x00, 0x0A, 0x01, 0x03};
+    sendBytesWithCRC(data0, 5);
+  uint8_t data1[] = {0x88, 0x00, 0x04, 0x01, 0xFF};
+    sendBytesWithCRC(data1, 5);
+    uint8_t data[] = {0x8E, 
+    0x00, 0x00, 0x00, 0x00,   //X
+    0x00, 0x00, 0x00, 0x00,   //Y
+    0x00, 0x00, 0x00, 0x00,   //Z
+    0x00, 0x00, 0x00, 0x00,   //A
+    0x00, 0x00, 0x00, 0x00,   //B
+   0x00, 0x00, 0x00, 0x00,    //duration
+   0x1F};                     // Relative or absolute
+int32_t temp = (((aSteps * rpm)/60) * duration);
+  if (dir == CW){temp = temp * -1;}
+    data[13] = (temp & 0x000000FF);
+    data[14] = (temp & 0x0000FF00)>>8;
+    data[15] = (temp & 0x00FF0000)>>16;
+    data[16] = (temp & 0xFF000000)>>24;
+    uint32_t temp1 = duration * 1000000;
+    data[21] = (temp1 & 0x000000FF);
+    data[22] = (temp1 & 0x0000FF00)>>8;
+    data[23] = (temp1 & 0x00FF0000)>>16;
+    data[24] = (temp1 & 0xFF000000)>>24;
+    sendBytesWithCRC(data,sizeof(data)); //26
+}
+
+void extendedStop(){
+    uint8_t data[] = {0x16,0x03};
+    sendBytesWithCRC(data, sizeof(data));
+}
+
+void reset(){
+    uint8_t data[] = {0x11};
+    sendBytesWithCRC(data, sizeof(data));
+}
+
+void disableSteppers(){
+  uint8_t data[] = {0x89, 0b00001111};
+  sendBytesWithCRC(data, 2);
+}
+
+void enableFan() {
+  uint8_t data[] = {0x0A,0x00,0x0C,0x01};
+  sendBytesWithCRC(data,  sizeof(data));
+}
+
+void disableFan() {
+  uint8_t data[] = {0x0A,0x00,0x0C,0x00};
+  sendBytesWithCRC(data, sizeof(data));
+}
+
+void coolFanON(bool state) {
+  if (state == true){
+    uint8_t data[] = {0x0A,0x00,0x0C,0x01};
+    sendBytesWithCRC(data, sizeof(data));
+  }
+  if (state == false){
+    uint8_t data[] = {0x0A,0x00,0x0C,0x00};
+    sendBytesWithCRC(data, sizeof(data));
+  }
+}
+
+void sendBytesWithCRC(uint8_t *data, int len){
+  uint8_t crc = 0x00;
+  int length = len;
+  int a;
+  int fn = 1;
+  commandSent = data[0];
+  Serial.write(0xD5);    // Send header Identifier byte
+  Serial.write(length);  // Send payload length
+  // Send Payload
+  for(int p = 0; p < length; p++){
+    Serial.write((uint8_t)data[p]);
+  }
+  // Send calculated CRC
+  for(a = 0; a < length; a++) {
+    //delay(1);
+    crc = calculateCRC(crc, data[a]);
+  }
+  Serial.write((uint8_t)crc);
+}
+
+uint8_t calculateCRC (uint8_t crc, uint8_t data) {
+  uint8_t cc;
+  crc = crc ^ data;
+  for (cc = 0; cc < 8; cc++) {
+    if (crc & 0x01)
+      crc = (crc >> 1) ^ 0x8C;
+    else
+      crc >>= 1;
+  }
+  return crc;
+}
+
+uint32_t read32(int axis) {
+  union {
+    // AVR is little-endian
+    int32_t a;
+    struct {
+      uint8_t data[4];
+    } 
+    b;
+  } 
+  shared;
+  shared.b.data[0] = currentPos[axis][0];
+  shared.b.data[1] = currentPos[axis][1];
+  shared.b.data[2] = currentPos[axis][2];
+  shared.b.data[3] = currentPos[axis][3];
+  return shared.a;
+}
+
+
+
